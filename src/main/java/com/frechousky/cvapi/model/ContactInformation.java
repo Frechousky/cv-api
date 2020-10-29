bd751957496805bb3cf7a126ed9270529a49ff0e
Frechousky <frechousky@gmail.com>
2020-10-29 22:58:54 +0100

    Replaced fullName by (firstName + lastName) and age by dateOfBirth on ContactInformation

--- src/main/java/com/frechousky/cvapi/model/ContactInformation.java
+++ src/main/java/com/frechousky/cvapi/model/ContactInformation.java
@@ -1,5 +1,6 @@
 package com.frechousky.cvapi.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Date;
 
 @Data
 @Builder
@@ -22,30 +24,33 @@ public class ContactInformation {
     @GeneratedValue
     private Integer id;
 
-    @NotBlank(message = "Contact details full name must not be blank")
-    private String fullName;
+    @NotBlank(message = "Contact information first name must not be blank")
+    private String firstName;
 
-    @NotBlank(message = "Contact details address must not be blank")
+    @NotBlank(message = "Contact information last name must not be blank")
+    private String lastName;
+
+    @NotBlank(message = "Contact information address must not be blank")
     private String address;
 
-    @NotBlank(message = "Contact details city must not be blank")
+    @NotBlank(message = "Contact information city must not be blank")
     private String city;
 
-    @NotBlank(message = "Contact details zip code must not be blank")
+    @NotBlank(message = "Contact information zip code must not be blank")
     private String zipCode;
 
-    @NotBlank(message = "Contact details phone must not be blank")
+    @NotBlank(message = "Contact information phone must not be blank")
     private String phone;
 
-    @NotBlank(message = "Contact details mail must not be blank")
+    @NotBlank(message = "Contact information mail must not be blank")
     @Email
     private String mail;
 
-    @NotNull(message = "Contact details age must not be not null")
-    @Min(value = 0, message = "Contact details age must be greater or equal to 0")
-    private Integer age;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @NotNull(message = "Contact information date of birth must not be null")
+    private Date dateOfBirth;
 
-    @NotBlank(message = "Contact details driver licence must not be blank")
+    @NotBlank(message = "Contact information driver licence must not be blank")
     private String driverLicence;
 
 }
