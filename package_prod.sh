a23714bdb209b3ec1948f88610a704e403536614
Frechousky <frechousky@gmail.com>
2021-05-14 14:28:13 +0200

    Add 'dev' and 'prod' profiles
    Add maven profiles 'dev' (default) and 'prod' (mvn ... -Pprod)
    Add 'application-dev.properties' and 'application-prod.properties'
    Add script to package 'dev' or 'prod' JAR

---
+++ package_prod.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+sudo docker run --rm \
+  -v $(pwd):/tmp \
+  -v $HOME/.m2:/root/.m2 \
+  -w /tmp \
+  --privileged \
+  maven:3.6.3-openjdk-11-slim \
+  mvn clean package -DskipTests=true -Pprod
+  
